afcbd1cd9b9a21edf288e342afaeb234b2aa1791
Dylan <dylan_verstraete1@hotmail.com>
2020-02-19 16:08:44 +0100

    moved folders

---
+++ ThreeBotPackages/threefoldfoundation/stellar-faucet/faucet-frontend/webpack.config.js
@@ -0,0 +1,31 @@
+// webpack.config.js
+
+module.exports = {
+  rules: [
+    {
+      test: /\.s(c|a)ss$/,
+      use: [
+        'vue-style-loader',
+        'css-loader',
+        {
+          loader: 'sass-loader',
+          // Requires sass-loader@^7.0.0
+          options: {
+            implementation: require('sass'),
+            fiber: require('fibers'),
+            indentedSyntax: true // optional
+          },
+          // Requires sass-loader@^8.0.0
+          // eslint-disable-next-line no-dupe-keys
+          options: {
+            implementation: require('sass'),
+            sassOptions: {
+              fiber: require('fibers'),
+              indentedSyntax: true // optional
+            },
+          },
+        },
+      ],
+    },
+  ]
+}
