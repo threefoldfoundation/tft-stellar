8143aae42108c906bb6aecd28ead524f1c1d8454
dylanverstraete <dylan_verstraete1@hotmail.com>
2020-02-20 16:39:50 +0100

    added testnet domain for threebotpackages

--- ThreeBotPackages/stellar-faucet/faucet-frontend/actions/fundAccount.js
+++ ThreeBotPackages/stellar-faucet/faucet-frontend/actions/fundAccount.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export function fundAccount(destination) {
-  return axios.post("http://localhost/threefoldfoundation/stellar_faucet/actors/stellar_faucet/transfer", { args: { destination } })
+  return axios.post("http://localhost/threefoldfoundation/stellar_faucet/transfer", { args: { destination } })
 }
