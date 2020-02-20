dcdf4d1dccf8c3e9bbfa65a094d2c4f2a564d343
dylanverstraete <dylan_verstraete1@hotmail.com>
2020-02-20 15:23:10 +0100

    rewriting location

--- ThreeBotPackages/unlock-service/package.py
+++ ThreeBotPackages/unlock-service/package.py
@@ -4,17 +4,13 @@ from Jumpscale import j
 class Package(j.baseclasses.threebot_package):
     def start(self):
         for port in (443, 80):
-            website = self.openresty.websites.get(f"unlock_service_{port}")
-            website.port = port
-            website.ssl = port == 443
+            website = self.openresty.get_from_port(port)
             locations = website.locations.get(name=f"unlock_service_{port}_locations")
 
-            include_location = locations.locations_custom.new()
-            include_location.name = f"unlock_service_includes_{port}"
-
+            include_location = locations.get_location_custom(f"unlock_service_includes_{port}")
             include_location.config = f"""
             location /threefoldfoundation/unlock_service {{
-                rewrite /threefoldfoundation/unlock_service/(.*)$ /threefoldfoundation/unlock_service/actors/unlock_service/$1
+                rewrite /threefoldfoundation/unlock_service/(.*)$ /threefoldfoundation/unlock_service/actors/unlock_service/$1;
             }}"""
 
             locations.configure()
