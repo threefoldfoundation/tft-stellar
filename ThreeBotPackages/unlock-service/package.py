f874029d66c38c8342540aaafff2ad3958233bce
dylanverstraete <dylan_verstraete1@hotmail.com>
2020-02-20 10:35:03 +0100

    try settings unlock service actor location

--- ThreeBotPackages/unlock-service/package.py
+++ ThreeBotPackages/unlock-service/package.py
@@ -5,4 +5,16 @@ class Package(j.baseclasses.threebot_package):
     def start(self):
         server = self.openresty
         server.install(reset=False)
+        port = 8901
+
+        website = self.openresty.websites.get(f"threefoldfoundation_{port}")
+        locations = website.locations.get(name=f"threefoldfoundation_{port}_locations")
+        include_location = locations.get_location_custom(f"threefoldfoundation_includes_{port}")
+
+        include_location.config = """
+        location /threefoldfoundation/unlock_service {
+            rewrite ^(.+) /threefoldfoundation/unlock_service/actors/unlock_service;
+        }"""
+
+        locations.configure()
         server.configure()
