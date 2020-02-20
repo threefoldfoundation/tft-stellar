fb7e101f6efa056977100056fcc881813ae7bc26
dylanverstraete <dylan_verstraete1@hotmail.com>
2020-02-20 11:33:02 +0100

    test

--- ThreeBotPackages/unlock-service/package.py
+++ ThreeBotPackages/unlock-service/package.py
@@ -6,19 +6,21 @@ class Package(j.baseclasses.threebot_package):
         server = self.openresty
         server.install(reset=False)
 
-        port = 80
+        for port in (443, 80):
+            website = server.get_from_port(port)
+            locations = website.locations.get()
+            include_location = locations.locations_custom.new()
+            include_location.name = f"unlock_service_{port}"
 
-        website = server.get_from_port(port)
-        locations = website.locations.get()
-        actor_location = locations.locations_custom.new()
-        actor_location.name = f"unlock_service_{port}"
+            default_website_name = self.openresty.get_from_port(port).name
 
-        actor_location.config = """
-        location /threefoldfoundation/unlock_service {
-            rewrite ^(.+) /threefoldfoundation/unlock_service/actors/unlock_service;
-        }"""
+            include_location.config = """
+            include {website.path_cfg_dir}/{default_website_name}_locations/*.conf;
+            location /threefoldfoundation/unlock_service {
+                rewrite ^(.+) /threefoldfoundation/unlock_service/actors/unlock_service;
+            }"""
 
-        locations.configure()
-        website.configure()
+            locations.configure()
+            website.configure()
 
         server.configure()
