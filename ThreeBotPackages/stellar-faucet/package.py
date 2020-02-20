7b3551ecb28785f32967edd844e3c49e108e9839
dylanverstraete <dylan_verstraete1@hotmail.com>
2020-02-20 17:01:16 +0100

    added testnet domain for threebotpackages

--- ThreeBotPackages/stellar-faucet/package.py
+++ ThreeBotPackages/stellar-faucet/package.py
@@ -1,9 +1,22 @@
-
 from Jumpscale import j
 
 
+DOMAIN = "testnet.threefoldtoken.io"
+
 class Package(j.baseclasses.threebot_package):
     def start(self):
-        server = self.openresty
-        server.install(reset=False)
-        server.configure()
+        for port in (443, 80):
+            website = self.openresty.get_from_port(port)
+            website.ssl = port == 443
+            website.domain = DOMAIN
+
+            locations = website.locations.get(name=f"stellar_faucet_{port}_locations")
+
+            include_location = locations.get_location_custom(f"stellar_faucet_includes_{port}")
+            include_location.config = f"""
+            location /threefoldfoundation/stellar_faucet {{
+                rewrite /threefoldfoundation/stellar_faucet/(.*)$ /threefoldfoundation/stellar_faucet/actors/stellar_faucet/$1;
+            }}"""
+
+            locations.configure()
+            website.configure()
--- ThreeBotPackages/unlock-service/package.py
+++ ThreeBotPackages/unlock-service/package.py
@@ -1,10 +1,15 @@
 from Jumpscale import j
 
 
+DOMAIN = "testnet.threefoldtoken.io"
+
 class Package(j.baseclasses.threebot_package):
     def start(self):
         for port in (443, 80):
             website = self.openresty.get_from_port(port)
+            website.ssl = port == 443
+            website.domain = DOMAIN
+
             locations = website.locations.get(name=f"unlock_service_{port}_locations")
 
             include_location = locations.get_location_custom(f"unlock_service_includes_{port}")
