9614b239c7536994c1a0a4d51b57d3658bb23871
dylanverstraete <dylan_verstraete1@hotmail.com>
2020-02-20 11:04:46 +0100

    adds ports 443 and 80

--- ThreeBotPackages/unlock-service/package.py
+++ ThreeBotPackages/unlock-service/package.py
@@ -5,17 +5,18 @@ class Package(j.baseclasses.threebot_package):
     def start(self):
         server = self.openresty
         server.install(reset=False)
-        port = 8901
 
-        website = server.get_from_port(port)
-        locations = website.locations.get()
-        actor_location = locations.locations_custom.new()
-        actor_location.name = "unlock_service"
+        for port in (443, 80):
+            website = server.get_from_port(port)
+            locations = website.locations.get()
+            actor_location = locations.locations_custom.new()
+            actor_location.name = "unlock_service"
 
-        actor_location.config = """
-        location /threefoldfoundation/unlock_service {
-            rewrite ^(.+) /threefoldfoundation/unlock_service/actors/unlock_service;
-        }"""
+            actor_location.config = """
+            location /threefoldfoundation/unlock_service {
+                rewrite ^(.+) /threefoldfoundation/unlock_service/actors/unlock_service;
+            }"""
 
-        locations.configure()
+            locations.configure()
+
         server.configure()
