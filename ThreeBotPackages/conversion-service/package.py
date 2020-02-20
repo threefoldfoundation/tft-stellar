3d3b06f6d49e97ae8bf0f34502182fc73a16304d
dylanverstraete <dylan_verstraete1@hotmail.com>
2020-02-20 15:31:18 +0100

    adds rewrite location for conversion service

--- ThreeBotPackages/conversion-service/package.py
+++ ThreeBotPackages/conversion-service/package.py
@@ -3,6 +3,15 @@ from Jumpscale import j
 
 class Package(j.baseclasses.threebot_package):
     def start(self):
-        server = self.openresty
-        server.install(reset=False)
-        server.configure()
+        for port in (443, 80):
+            website = self.openresty.get_from_port(port)
+            locations = website.locations.get(name=f"conversion_service_{port}_locations")
+
+            include_location = locations.get_location_custom(f"conversion_service_includes_{port}")
+            include_location.config = f"""
+            location /threefoldfoundation/conversion_service {{
+                rewrite /threefoldfoundation/conversion_service/(.*)$ /threefoldfoundation/conversion_service/actors/conversion_service/$1;
+            }}"""
+
+            locations.configure()
+            website.configure()
