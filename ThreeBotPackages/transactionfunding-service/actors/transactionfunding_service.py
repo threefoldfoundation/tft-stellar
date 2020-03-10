1ae59fdb054db5a500e8315d5cb7faac1ce993ce
Rob Van Mieghem <robvanmieghem@gmail.com>
2020-03-10 14:27:46 +0100

    Charge 0.1 TFT for funding a transaction
    Solves #35

--- ThreeBotPackages/transactionfunding-service/actors/transactionfunding_service.py
+++ ThreeBotPackages/transactionfunding-service/actors/transactionfunding_service.py
@@ -1,38 +1,64 @@
 from Jumpscale import j
 
 
-_TFT_FULL_ASSETCODES={"TEST":"TFT:GA47YZA3PKFUZMPLQ3B5F2E3CJIB57TGGU7SPCQT2WAEYKN766PWIMB3","STD":"TFT:issuertobefilledin"}
+_TFT_FULL_ASSETCODES = {
+    "TEST": "TFT:GA47YZA3PKFUZMPLQ3B5F2E3CJIB57TGGU7SPCQT2WAEYKN766PWIMB3",
+    "STD": "TFT:issuertobefilledin",
+}
 _HORIZON_NETWORKS = {"TEST": "https://horizon-testnet.stellar.org", "STD": "https://horizon.stellar.org"}
+
+
 class transactionfunding_service(j.baseclasses.threebot_actor):
-
-
     def _get_horizon_server(self, network):
         import stellar_sdk
+
         return stellar_sdk.Server(horizon_url=_HORIZON_NETWORKS[str(network)])
 
+    def _get_tft_asset(self, network):
+        import stellar_sdk
+
+        fullcode = _TFT_FULL_ASSETCODES[str(network)]
+        split_code = fullcode.split(":")
+        return stellar_sdk.Asset(split_code[0], split_code[1])
+
+    def _create_tft_fee_payment(self, from_address, destination, network):
+        import stellar_sdk
+
+        asset = self._get_tft_asset(network)
+        return stellar_sdk.Payment(destination, asset, "0.1", from_address)
+
     @j.baseclasses.actor_method
-    def fund_transaction(self, transaction,schema_out=None, user_session=None):
+    def fund_transaction(self, transaction, schema_out=None, user_session=None):
 
         funding_wallet = j.clients.stellar.get("txfundingwallet")
-        
-        #after getting the wallet, the required imports are available
+
+        # after getting the wallet, the required imports are available
         import stellar_sdk
-        if str(funding_wallet.network) == 'TEST':
-            network_passphrase= stellar_sdk.Network.TESTNET_NETWORK_PASSPHRASE
+
+        if str(funding_wallet.network) == "TEST":
+            network_passphrase = stellar_sdk.Network.TESTNET_NETWORK_PASSPHRASE
         else:
-           network_passphrase= stellar_sdk.Network.PUBLIC_NETWORK_PASSPHRASE 
-        txe= stellar_sdk.transaction_envelope.TransactionEnvelope.from_xdr(transaction,network_passphrase)
-        source_public_kp=stellar_sdk.Keypair.from_public_key(funding_wallet.address)
-        source_signing_kp=stellar_sdk.Keypair.from_secret(funding_wallet.secret)
-        horizon_server=self._get_horizon_server(funding_wallet.network)
-        base_fee=horizon_server.fetch_base_fee()
+            network_passphrase = stellar_sdk.Network.PUBLIC_NETWORK_PASSPHRASE
+        txe = stellar_sdk.transaction_envelope.TransactionEnvelope.from_xdr(transaction, network_passphrase)
+        source_public_kp = stellar_sdk.Keypair.from_public_key(funding_wallet.address)
+        source_signing_kp = stellar_sdk.Keypair.from_secret(funding_wallet.secret)
+        horizon_server = self._get_horizon_server(funding_wallet.network)
+        base_fee = horizon_server.fetch_base_fee()
 
-        source_account=horizon_server.load_account(funding_wallet.address)
+        source_account = horizon_server.load_account(funding_wallet.address)
         source_account.increment_sequence_number()
-        txe.transaction.source=source_public_kp
+        txe.transaction.source = source_public_kp
 
-        txe.transaction.fee= base_fee * len(txe.transaction.operations)
-        
-        txe.transaction.sequence=source_account.sequence
+        if len(txe.transaction.operations) == 0:
+            raise j.exceptions.Base("No operations in the supplied transaction")
+        txe.transaction.operations.append(
+            self._create_tft_fee_payment(
+                txe.transaction.operations[0].source, funding_wallet.address, funding_wallet.network
+            )
+        )
+
+        txe.transaction.fee = base_fee * len(txe.transaction.operations)
+
+        txe.transaction.sequence = source_account.sequence
         txe.sign(source_signing_kp)
         return txe.to_xdr()
--- ThreeBotPackages/transactionfunding-service/testscripts/createunfundedpayment.py
+++ ThreeBotPackages/transactionfunding-service/testscripts/createunfundedpayment.py
@@ -3,34 +3,32 @@
 
 
 import stellar_sdk
-from stellar_sdk.transaction_builder import Transaction, TransactionEnvelope,Asset, Payment
+from stellar_sdk.transaction_builder import Transaction, TransactionEnvelope, Asset, Payment
 import click
 
+
 @click.command()
-@click.argument('destination', type=str)
-@click.option('--amount', type=str,default='1')
-@click.option('--asset',default='TFT:GA47YZA3PKFUZMPLQ3B5F2E3CJIB57TGGU7SPCQT2WAEYKN766PWIMB3')
-@click.option('--from_address', type=str, required=True)
-def createpayment_transaction(destination,asset,amount,from_address):
-    split_asset= asset.split(':',1)
-    asset_code=split_asset[0]
-    asset_issuer=split_asset[1]
+@click.argument("destination", type=str)
+@click.option("--amount", type=str, default="1")
+@click.option("--asset", default="TFT:GA47YZA3PKFUZMPLQ3B5F2E3CJIB57TGGU7SPCQT2WAEYKN766PWIMB3")
+@click.option("--from_address", type=str, required=True)
+def createpayment_transaction(destination, asset, amount, from_address):
+    split_asset = asset.split(":", 1)
+    asset_code = split_asset[0]
+    asset_issuer = split_asset[1]
 
     asset = Asset(code=asset_code, issuer=asset_issuer)
     # Create the transaction
 
     op = Payment(destination, asset, amount, from_address)
     transaction = Transaction(
-            source=stellar_sdk.Keypair.from_public_key(from_address),
-            sequence=1,
-            fee=0,
-            operations=[op],
-        )
+        source=stellar_sdk.Keypair.from_public_key(from_address), sequence=1, fee=0, operations=[op]
+    )
     transaction_envelope = TransactionEnvelope(
-            transaction=transaction, network_passphrase=stellar_sdk.Network.TESTNET_NETWORK_PASSPHRASE
-        )
+        transaction=transaction, network_passphrase=stellar_sdk.Network.TESTNET_NETWORK_PASSPHRASE
+    )
     print(transaction_envelope.to_xdr())
 
 
-if __name__ == '__main__':
-  createpayment_transaction()
+if __name__ == "__main__":
+    createpayment_transaction()
