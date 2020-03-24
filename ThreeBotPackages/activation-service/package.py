3ab91103f94a482590f8a65bfd855543fb41b98d
Dylan Verstraete <dylan_verstraete1@hotmail.com>
2020-03-24 20:49:29 +0100

    Fix domain configuration issue
    * fixed package install

--- ThreeBotPackages/activation-service/package.py
+++ ThreeBotPackages/activation-service/package.py
@@ -3,11 +3,11 @@ from Jumpscale import j
 
 class Package(j.baseclasses.threebot_package):
     def start(self):
-        #DOMAIN = self.install_kwargs.get("domain") or "testnet.threefoldtoken.io"
+        DOMAIN = self._package.install_kwargs.get("domain") or "testnet.threefoldtoken.io"
         for port in (443, 80):
             website = self.openresty.get_from_port(port)
             website.ssl = port == 443
-            #website.domain = DOMAIN
+            website.domain = DOMAIN
 
             locations = website.locations.get(name=f"activation_service_{port}_locations")
 
