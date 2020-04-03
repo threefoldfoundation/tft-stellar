1a1b928d792baf9dcb8d3c3cf412e6fe125b292b
dylanVerstraete <dylan_verstraete1@hotmail.com>
2020-04-03 11:53:49 +0200

    adds auth to faucet actor

--- ThreeBotPackages/stellar-faucet/actors/stellar_faucet.py
+++ ThreeBotPackages/stellar-faucet/actors/stellar_faucet.py
@@ -1,9 +1,15 @@
 from Jumpscale import j
+from urllib.request import urlopen
+import nacl
+import json
 
 
 class stellar_faucet(j.baseclasses.threebot_actor):
     @j.baseclasses.actor_method
-    def transfer(self, destination, username, schema_out=None, user_session=None):
+    def transfer(self, destination, username, signed_hash, schema_out=None, user_session=None):
+        if not self.is_3bot_user(username, signed_hash):
+            raise Exception("not a valid user")
+        
         distributor = j.clients.stellar.get("distributor")
 
         distributor.network = self.package.install_kwargs.get("network")
@@ -18,6 +24,19 @@ class stellar_faucet(j.baseclasses.threebot_actor):
                 raise Exception("user already requested token")
 
         try:
-            distributor.transfer(destination_address=destination, amount=amount, asset=asset, memo_text=username)
+            distributor.transfer(destination_address=destination, amount=amount, asset=asset, memo_text=double_name)
         except Exception as e:
             raise e
+
+    def is_3bot_user(self, username, signed_hash):
+        auth_response = urlopen("https://login.threefold.me/api/users/{}".format(double_name))
+        data = json.loads(auth_response.read())
+        user_public = data['publicKey']
+
+        verify_key = nacl.signing.VerifyKey(user_public, encoder=nacl.encoding.Base64Encoder)
+
+        try:
+            verify_key.verify(base64.b64decode(signed_hash))
+        except:
+            return False
+        return True
