71b5d682dab2aaf3b53d0f5d3c6010b35e02cdc4
dylanVerstraete <dylan_verstraete1@hotmail.com>
2020-04-07 11:46:26 +0200

    rename package

--- ThreeBotPackages/stellar_faucet/faucet_frontend/vue.config.js
+++ ThreeBotPackages/stellar_faucet/faucet_frontend/vue.config.js
@@ -2,14 +2,8 @@ const path = require("path");
 const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
 
 module.exports = {
-  devServer: {
-    host: '0.0.0.0',
-    port: '8080',
-    public: '0.0.0.0:8080',
-    disableHostCheck: true,
-  },
   outputDir: path.resolve(__dirname, "../frontend"),
-  publicPath: '',
+  publicPath: '/threefoldfoundation/stellar_faucet',
   configureWebpack: {
     plugins: [
       new VuetifyLoaderPlugin()
