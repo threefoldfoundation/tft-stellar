c82adc3ccd5ad9f07116f043dc95e3b4cf8fbbaa
dylanVerstraete <dylan_verstraete1@hotmail.com>
2020-04-07 10:03:59 +0200

    fix location config in stellar faucet

--- ThreeBotPackages/stellar-faucet/package.py
+++ ThreeBotPackages/stellar-faucet/package.py
@@ -8,10 +8,4 @@ class Package(j.baseclasses.threebot_package):
             website = self.openresty.get_from_port(port)
             website.ssl = port == 443
             website.domain = DOMAIN
-
-            locations = website.locations.get(name=f"stellar_faucet_{port}_locations")
-
-            include_location = locations.get_location_custom(f"stellar_faucet_includes_{port}")
-
-            locations.configure()
             website.configure()
