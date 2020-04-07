0f5f6d0579761b825fc77943450b00ad674e8043
dylanVerstraete <dylan_verstraete1@hotmail.com>
2020-04-07 08:42:49 +0200

    remove url rewrite in stellar faucet actor, closes #73

--- ThreeBotPackages/stellar-faucet/package.py
+++ ThreeBotPackages/stellar-faucet/package.py
@@ -12,10 +12,6 @@ class Package(j.baseclasses.threebot_package):
             locations = website.locations.get(name=f"stellar_faucet_{port}_locations")
 
             include_location = locations.get_location_custom(f"stellar_faucet_includes_{port}")
-            include_location.config = f"""
-            location /threefoldfoundation/stellar_faucet {{
-                rewrite /threefoldfoundation/stellar_faucet/(.*)$ /threefoldfoundation/stellar_faucet/actors/stellar_faucet/$1;
-            }}"""
 
             locations.configure()
             website.configure()
