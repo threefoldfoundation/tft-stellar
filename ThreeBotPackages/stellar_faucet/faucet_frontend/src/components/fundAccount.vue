d1dc033d23e90c6c8ce7292fc5783fab7626d123
dylanVerstraete <dylan_verstraete1@hotmail.com>
2020-04-09 12:09:56 +0200

    fixed bug in faucet and updated frontend dir

--- ThreeBotPackages/stellar_faucet/faucet_frontend/src/components/fundAccount.vue
+++ ThreeBotPackages/stellar_faucet/faucet_frontend/src/components/fundAccount.vue
@@ -9,24 +9,23 @@
           justify="center"
         >
           <v-col
-            cols="4"
-            lg="12"
-            md="10"
-            sm="6"
+            cols="10"
+            sm="12"
+            md="6"
           >
-            <h1>FreeTFT Faucet</h1>
+            <h1>Stellar Faucet</h1>
             <v-card class="elevation-12">
               <v-toolbar
                 color="primary"
                 dark
                 flat
               >
-                <v-toolbar-title class="title">Fund a FreeTFT address</v-toolbar-title>
+                <v-toolbar-title>Fund a Stellar address</v-toolbar-title>
               </v-toolbar>
               <v-card-text>
                 <v-form v-on:submit.prevent="fundAddress">
                   <v-text-field v-model="address" required placeholder="Address" />
-                  <p v-if="error" id="errortext">This address probably does not exist or does not have a trustline with the issuer of our Stellar TFT (Testnet).</p>
+                  <p v-if="error" id="errortext">This address probably does not exist or does not have a trustline with the issuer of our Stellar FreeTFT (Testnet). Or already requested tokens before!</p>
                 </v-form>
               </v-card-text>
               <v-card-actions>
@@ -41,14 +40,6 @@
                 </v-btn>
               </v-card-actions>
             </v-card>
-            <br>
-            <br>
-            <v-card class="elevation-12">
-              <v-card-text>
-                <p>Here you can request FreeTFT, you can use this FreeTFT to pay for free capacity. Fill in a valid stellar address that has a trustline to the issuer of FreeTFT tokens.</p>
-              </v-card-text>
-            </v-card>
-            
           </v-col>
         </v-row>
       </v-container>
@@ -79,17 +70,18 @@ export default {
       console.log('Error: ', error)
       return
     }
-
-    let signedAttemptObject = JSON.parse(url.searchParams.get('signedAttempt'));
-    this.signedAttemptObject = signedAttemptObject
   },
   methods: {
     fundAddress() {
       this.loading = true
       this.error = false
-      
-      console.log(this.signedAttemptObject)
-      fundAccount(this.address, this.signedAttemptObject)
+
+      let url = new URL(window.location.href)
+      const search = url.hash.split("?")[1]
+      let params = new URLSearchParams(search);
+      const obj = params.get("signedAttempt");
+      const signedAttemptObject = JSON.parse(obj)
+      fundAccount(this.address, signedAttemptObject)
         .then(res => {
           if (res.status == 200) {
             this.loading = false
@@ -98,7 +90,8 @@ export default {
             this.error = true
           }
         })
-        .catch(() => {
+        .catch(err => {
+          console.log(err.meta)
           this.error = true
           this.loading = false
         })
@@ -113,19 +106,5 @@ export default {
 }
 #inspire {
   width: 1200px;
-}
-h1 {
-  font-family: 'Bebas Neue', cursive;
-  color: #1072ba
-}
-p {
-  font-family: 'Lato', sans-serif;
-}
-.title {
-  font-family: 'Lato', sans-serif;
-}
-.logo {
-  width: 40px;
-  height: 40px;
 }
 </style>
