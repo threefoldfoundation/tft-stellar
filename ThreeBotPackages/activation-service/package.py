264a486c100138357a6489900a56f02def8528eb
Rob Van Mieghem <robvanmieghem@gmail.com>
2020-05-03 08:10:41 +0200

    High level gevent flow in activation service

--- ThreeBotPackages/activation-service/package.py
+++ ThreeBotPackages/activation-service/package.py
@@ -3,6 +3,11 @@ from Jumpscale import j
 
 class Package(j.baseclasses.threebot_package):
     def start(self):
+
+        activation_wallet_name= = self._package.install_kwargs.get("wallet","activation_wallet")
+        
+        self.activation_wallet= j.clients.stellar.get(activation_wallet_name)
+
         DOMAIN = self._package.install_kwargs.get("domain") or "testnet.threefoldtoken.io"
         for port in (443, 80):
             website = self.openresty.get_from_port(port)
@@ -13,10 +18,39 @@ class Package(j.baseclasses.threebot_package):
 
             include_location = locations.get_location_custom(f"activation_service_includes_{port}")
             include_location.is_auth = False
-            include_location.config = f"""
-            location /threefoldfoundation/activation_service {{
+            include_location.config = """
+            location /threefoldfoundation/activation_service {
                 rewrite /threefoldfoundation/activation_service/(.*)$ /threefoldfoundation/activation_service/actors/activation_service/$1;
-            }}"""
+            }"""
 
             locations.configure()
             website.configure()
+         self._start_activation_loop()
+
+    def _start_activation_loop():
+        print("Starting activation service loop")
+        # create gevent queueu
+        self.queue = gevent.queue.Queue()
+        # start gevent loop at _funding_loop
+        self._activation_greenlet = gevent.spawn(self._activation_loop)
+
+    def _activation_loop(self)
+        for address in self.queue:
+            try:
+                self.activation_wallet.activate_account(address, starting_balance="3.6")
+                 #TODO signal caller
+            except Exception e:
+                #TODO: return exception to caller
+    
+    def activate_account(self, address):
+         # add address to gevent queue
+        self.queue.put(address)
+        #TODO wait for processing to finish
+
+
+    def _stop_activation_loop(self):
+        print("Halting activation service loop")
+        self._activation_greenlet.kill()
+
+    def stop(self):
+        self._stop_activation_loop()
