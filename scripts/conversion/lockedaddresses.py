579b9a4094d876ddb28ae5224706af3016e252da
Rob Van Mieghem <robvanmieghem@gmail.com>
2020-05-22 18:23:37 +0200

    list deauthorized addresses

---
+++ scripts/conversion/lockedaddresses.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# pylint: disable=no-value-for-parameter
+
+import click
+import requests
+import json
+import time
+
+TFCHAIN_EXPLORER="https://explorer2.threefoldtoken.com"
+
+
+@click.command(
+    help="List locked tfchain addresses with the locking transaction"
+)
+def list_locked_addresses():
+
+    response=requests.get(TFCHAIN_EXPLORER+"/explorer")
+    j= response.json()
+    currentheight=j['height']
+
+    while currentheight> 500000:
+        response=requests.get(TFCHAIN_EXPLORER+f"/explorer/blocks/{currentheight}")
+        j=response.json()
+        for transaction in j['block']['transactions']:
+            if transaction['rawtransaction']['version']!=176:
+                continue
+            for address in transaction['rawtransaction']['data']['deauthaddresses']:
+                print(f"{transaction['id']} {address}")
+        time.sleep(2)
+        currentheight-=1
+
+
+if __name__ == "__main__":
+    list_locked_addresses()
---
+++ scripts/conversion/readme.txt
@@ -0,0 +1,9 @@
+# Scripts to see the status od the conversion
+
+## List locked accounts
+
+```sh
+./lockedaddresses.py > deauthorizations.txt
+```
+
+This will create a file with a `transactionid tfchainaddress` combination per line.
