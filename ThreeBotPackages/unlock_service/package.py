3eb2f39921d258ba2c54f12e4e116e4a10306fc8
Rob Van Mieghem <robvanmieghem@gmail.com>
2020-12-04 09:44:12 +0100

    Make the unlock service installation more resilient

--- ThreeBotPackages/unlock_service/package.py
+++ ThreeBotPackages/unlock_service/package.py
@@ -5,15 +5,19 @@ from jumpscale.loader import j
 
 class unlock_service:
     def install(self, **kwargs):
-        location_actors_443 = j.sals.nginx.main.websites.default_443.locations.get(name="unlock_service_actors")
-        location_actors_443.is_auth = False
-        location_actors_443.is_admin = False
-        location_actors_443.save()
 
-        location_actors_80 = j.sals.nginx.main.websites.default_80.locations.get(name="unlock_service_actors")
-        location_actors_80.is_auth = False
-        location_actors_80.is_admin = False
-        location_actors_80.save()
+        if "default_443" in j.sals.nginx.main.websites.list_all():
+            location_actors_443 = j.sals.nginx.main.websites.default_443.locations.get(name="unlock_service_actors")
+            location_actors_443.is_auth = False
+            location_actors_443.is_admin = False
+            location_actors_443.save()
+
+        if "default_80" in j.sals.nginx.main.websites.list_all(): 
+            location_actors_80 = j.sals.nginx.main.websites.default_80.locations.get(name="unlock_service_actors")
+            location_actors_80.is_auth = False
+            location_actors_80.is_admin = False
+            location_actors_80.save()
+
         # Configure server domain (passed as kwargs if not, will be the default domain in package.toml)
         if "domain" in kwargs:
             domain = kwargs.get("domain")
@@ -26,8 +30,11 @@ class unlock_service:
             j.sals.nginx.main.websites.get(f"{package_name}_{server_name}_80").domain = domain
             j.sals.nginx.main.websites.get(f"{package_name}_{server_name}_80").configure()
 
-        j.sals.nginx.main.websites.default_443.configure()
-        j.sals.nginx.main.websites.default_80.configure()
+        if "default_443" in j.sals.nginx.main.websites.list_all():
+            j.sals.nginx.main.websites.default_443.configure()
+        
+        if "default_80" in j.sals.nginx.main.websites.list_all(): 
+            j.sals.nginx.main.websites.default_80.configure()
 
     def start(self, **kwargs):
         self.install(**kwargs)
