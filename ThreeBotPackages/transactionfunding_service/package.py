ab87cf5fd4f76890a4aedff0fb4d190b9770af45
Rob Van Mieghem <robvanmieghem@gmail.com>
2020-12-10 09:47:41 +0100

    Make the tx funding package installation more resilient

--- ThreeBotPackages/transactionfunding_service/package.py
+++ ThreeBotPackages/transactionfunding_service/package.py
@@ -42,15 +42,17 @@ class transactionfunding_service:
         nr_of_slaves = kwargs.get("slaves", 30)
         ensure_slavewallets(nr_of_slaves)
 
-        location_actors_443 = j.sals.nginx.main.websites.default_443.locations.get(name="transactionfunding_actors")
-        location_actors_443.is_auth = False
-        location_actors_443.is_admin = False
-        location_actors_443.save()
+        if "default_443" in j.sals.nginx.main.websites.list_all():
+            location_actors_443 = j.sals.nginx.main.websites.default_443.locations.get(name="transactionfunding_actors")
+            location_actors_443.is_auth = False
+            location_actors_443.is_admin = False
+            location_actors_443.save()
 
-        location_actors_80 = j.sals.nginx.main.websites.default_80.locations.get(name="transactionfunding_actors")
-        location_actors_80.is_auth = False
-        location_actors_80.is_admin = False
-        location_actors_80.save()
+        if "default_80" in j.sals.nginx.main.websites.list_all(): 
+            location_actors_80 = j.sals.nginx.main.websites.default_80.locations.get(name="transactionfunding_actors")
+            location_actors_80.is_auth = False
+            location_actors_80.is_admin = False
+            location_actors_80.save()
 
         # Configure server domain (passed as kwargs if not, will be the default domain in package.toml)
         if "domain" in kwargs:
@@ -64,8 +66,11 @@ class transactionfunding_service:
             j.sals.nginx.main.websites.get(f"{package_name}_{server_name}_80").domain = domain
             j.sals.nginx.main.websites.get(f"{package_name}_{server_name}_80").configure()
 
-        j.sals.nginx.main.websites.default_443.configure()
-        j.sals.nginx.main.websites.default_80.configure()
+        if "default_443" in j.sals.nginx.main.websites.list_all():
+            j.sals.nginx.main.websites.default_443.configure()
+
+        if "default_80" in j.sals.nginx.main.websites.list_all(): 
+            j.sals.nginx.main.websites.default_80.configure()
 
         start_funding_loop()
 
