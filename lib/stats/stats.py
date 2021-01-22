716d316ba0ee167605290f5df8b3ef9b180c5b39
Rob Van Mieghem <robvanmieghem@gmail.com>
2021-01-22 08:36:21 +0100

    Refactor the statistics lib for some functions to be used apart

--- lib/stats/stats.py
+++ lib/stats/stats.py
@@ -28,9 +28,61 @@ _NETWORK_PASSPHRASES = {
 }
 
 
+def get_horizon_server(network):
+    server_url = _HORIZON_NETWORKS[network]
+    return stellar_sdk.Server(horizon_url=server_url)
+
+
+def get_url(network, endpoint):
+    url = _THREEFOLDFOUNDATION_TFTSTELLAR_SERVICES[network]
+    return f"https://{url}{endpoint}"
+
+
+def get_unlockhash_transaction(network, unlockhash):
+    data = {"unlockhash": unlockhash}
+    resp = j.tools.http.post(
+        get_url(network, "/threefoldfoundation/unlock_service/get_unlockhash_transaction"), json={"args": data}
+    )
+    resp.raise_for_status()
+    return resp.json()
+
+
+def get_locked_accounts(network, tokencode: str, issuer: str):
+    locked_accounts = []
+    horizon_server = get_horizon_server(network)
+    asset = stellar_sdk.Asset(tokencode, issuer)
+    accounts_endpoint = horizon_server.accounts().for_asset(asset).limit(50)
+    old_cursor = "old"
+    new_cursor = ""
+    while new_cursor != old_cursor:
+        old_cursor = new_cursor
+        accounts_endpoint.cursor(new_cursor)
+        response = accounts_endpoint.call()
+        next_link = response["_links"]["next"]["href"]
+        next_link_query = parse.urlsplit(next_link).query
+        new_cursor = parse.parse_qs(next_link_query)["cursor"][0]
+        accounts = response["_embedded"]["records"]
+        for account in accounts:
+            account_id = account["account_id"]
+            preauth_signers = [signer["key"] for signer in account["signers"] if signer["type"] == "preauth_tx"]
+            tokenbalances = [
+                float(b["balance"])
+                for b in account["balances"]
+                if b["asset_type"] == "credit_alphanum4"
+                and b["asset_code"] == tokencode
+                and b["asset_issuer"] == issuer
+            ]
+            tokenbalance = tokenbalances[0] if tokenbalances else 0
+            if len(preauth_signers) > 0:
+                locked_accounts.append(
+                    {"account": account_id, "amount": tokenbalance, "preauth_signers": preauth_signers}
+                )
+    return locked_accounts
+
+
 class StatisticsCollector(object):
     """
-    Gathers statistics on TFT, TFTA and FreeTFT.
+    Gathers statistics on TFT and TFTA.
     Reuse an instance of this class to cache the locktimes of escrow accounts and speed up the detailed statistics gathering
     """
 
@@ -39,62 +91,11 @@ class StatisticsCollector(object):
         self.locked_accounts = {}
 
     @property
-    def _horizon_server(self):
-        server_url = _HORIZON_NETWORKS[self._network]
-        return stellar_sdk.Server(horizon_url=server_url)
-
-    @property
     def _network_passphrase(self):
         return _NETWORK_PASSPHRASES[self._network]
 
-    def get_accounts(self, tokencode: str, issuer: str):
-        locked_accounts = []
-        horizon_server = self._horizon_server
-        asset = stellar_sdk.Asset(tokencode, issuer)
-        accounts_endpoint = horizon_server.accounts().for_asset(asset).limit(50)
-        old_cursor = "old"
-        new_cursor = ""
-        while new_cursor != old_cursor:
-            old_cursor = new_cursor
-            accounts_endpoint.cursor(new_cursor)
-            response = accounts_endpoint.call()
-            next_link = response["_links"]["next"]["href"]
-            next_link_query = parse.urlsplit(next_link).query
-            new_cursor = parse.parse_qs(next_link_query)["cursor"][0]
-            accounts = response["_embedded"]["records"]
-            for account in accounts:
-                account_id = account["account_id"]
-                preauth_signers = [signer["key"] for signer in account["signers"] if signer["type"] == "preauth_tx"]
-                tokenbalances = [
-                    float(b["balance"])
-                    for b in account["balances"]
-                    if b["asset_type"] == "credit_alphanum4"
-                    and b["asset_code"] == tokencode
-                    and b["asset_issuer"] == issuer
-                ]
-                tokenbalance = tokenbalances[0] if tokenbalances else 0
-                if len(preauth_signers) > 0:
-                    locked_accounts.append(
-                        {"account": account_id, "amount": tokenbalance, "preauth_signers": preauth_signers}
-                    )
-        return locked_accounts
-
-    def _get_url(self, endpoint):
-        url = _THREEFOLDFOUNDATION_TFTSTELLAR_SERVICES[self._network]
-        if not j.sals.nettools.wait_connection_test(url, 443, 5):
-            raise j.exceptions.Timeout(f"Can not connect to server {url}, connection timeout")
-        return f"https://{url}{endpoint}"
-
-    def _get_unlockhash_transaction(self, unlockhash):
-        data = {"unlockhash": unlockhash}
-        resp = j.tools.http.post(
-            self._get_url("/threefoldfoundation/unlock_service/get_unlockhash_transaction"), json={"args": data}
-        )
-        resp.raise_for_status()
-        return resp.json()
-
     def lookup_lock_time(self, preauth_signer: str):
-        unlock_tx = self._get_unlockhash_transaction(unlockhash=preauth_signer)
+        unlock_tx = get_unlockhash_transaction(self._network,unlockhash=preauth_signer)
         if unlock_tx is None:
             return None
         txe = stellar_sdk.TransactionEnvelope.from_xdr(unlock_tx["transaction_xdr"], self._network_passphrase)
@@ -105,14 +106,14 @@ class StatisticsCollector(object):
 
     def getstatistics(self, tokencode: str, detailed: bool):
         stats = {"asset": tokencode}
-        horizon_server = self._horizon_server
+        horizon_server = get_horizon_server(self._network)
         asset_issuer = _ASSET_ISUERS[self._network][tokencode]
         stats["issuer"] = asset_issuer
         response = horizon_server.assets().for_code(tokencode).for_issuer(asset_issuer).call()
         record = response["_embedded"]["records"][0]
         stats["total"] = float(record["amount"])
         stats["num_accounts"] = record["num_accounts"]
-        locked_accounts = self.get_accounts(tokencode, asset_issuer)
+        locked_accounts = get_locked_accounts(self._network, tokencode, asset_issuer)
         total_locked = 0.0
         for locked_account in locked_accounts:
             total_locked += locked_account["amount"]
@@ -143,11 +144,11 @@ class StatisticsCollector(object):
 
 
 @click.command(help="Statistics about TFT, TFTA and FreeTFT")
-@click.argument("tokencode", type=click.Choice(["TFT", "TFTA", "FreeTFT"]), default="TFT")
+@click.argument("tokencode", type=click.Choice(["TFT", "TFTA"]), default="TFT")
 @click.option("--network", type=click.Choice(["test", "public"], case_sensitive=False), default="public")
 @click.option("--detailed/--not-detailed", default=False)
 def show_stats(tokencode, network, detailed):
-    print(f"Statistics for {tokencode} on the {network} network")
+    print(f"Gathering statistics for {tokencode} on the {network} network")
     collector = StatisticsCollector(network)
     stats = collector.getstatistics(tokencode, detailed)
     print(f"Total amount of tokens: {stats['total']:,.7f}")
