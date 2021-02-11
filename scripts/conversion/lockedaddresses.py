e717cde05ecdd9ccac89f1e4a5ab5d686f961359
Rob Van Mieghem <robvanmieghem@gmail.com>
2021-02-11 15:55:17 +0100

    Update tfchain deauthorizations

--- scripts/conversion/lockedaddresses.py
+++ scripts/conversion/lockedaddresses.py
@@ -5,21 +5,27 @@ import click
 import requests
 import json
 import time
+import sys
+import faulthandler
 
 TFCHAIN_EXPLORER = "https://explorer2.threefoldtoken.com"
 
 
 @click.command(help="List locked tfchain addresses with the locking transaction")
-def list_locked_addresses():
-
-    response = requests.get(TFCHAIN_EXPLORER + "/explorer")
-    j = response.json()
-    currentheight = j["height"]
+@click.option("--startingheight",default=-1,type=int)
+def list_locked_addresses(startingheight):
+    currentheight=startingheight
+    if currentheight<=0:
+        response = requests.get(TFCHAIN_EXPLORER + "/explorer")
+        j = response.json()
+        currentheight = j["height"]
 
     while currentheight > 500000:
+        print(currentheight,file=sys.stderr)
         try:
             response = requests.get(TFCHAIN_EXPLORER + f"/explorer/blocks/{currentheight}")
-        except requests.exceptions.ConnectionError:
+        except requests.exceptions.ConnectionError as e:
+            print (e, file=sys.stderr)
             time.sleep(2)
             continue
         j = response.json()
@@ -28,9 +34,10 @@ def list_locked_addresses():
                 continue
             for address in transaction["rawtransaction"]["data"]["deauthaddresses"]:
                 print(f"{transaction['id']} {address}")
-        time.sleep(2)
+        time.sleep(1)
         currentheight -= 1
 
 
 if __name__ == "__main__":
+    faulthandler.enable()
     list_locked_addresses()
