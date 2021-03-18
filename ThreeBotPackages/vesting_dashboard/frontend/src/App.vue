49f85e71922e6cc3a5fbc46cc8dbb0d89248d6b7
samaradel <samar@incubaid.com>
2021-03-18 17:25:46 +0200

    Init vesting UI

---
+++ ThreeBotPackages/vesting_dashboard/frontend/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container class="my-5">
+        <v-img
+          lazy-src="./assets/3bot_robot.png"
+          height="auto"
+          width="150"
+          src="./assets/3bot_robot.png"
+          class="mx-auto mt-5"
+        ></v-img>
+        <VestingForm class="mb-16" />
+
+        <AccountTable />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import VestingForm from "./components/VestingForm";
+import AccountTable from "./components/AccountTable";
+export default {
+  name: "App",
+  components: {
+    VestingForm,
+    AccountTable,
+  },
+};
+</script>
---
+++ ThreeBotPackages/vesting_dashboard/frontend/src/components/VestingForm.vue
@@ -0,0 +1,64 @@
+
+<template>
+  <v-form ref="form" v-model="valid" lazy-validation @submit.prevent="submit">
+    <v-row>
+      <v-col sm="12">
+        <v-text-field
+          label="Address"
+          :rules="addressRules"
+          v-model="address"
+          :loading="loading"
+        ></v-text-field>
+        <v-btn class="primary" type="submit" @click="validate">
+          create vesting account
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-form>
+</template>
+
+<script>
+import VestingServices from "../services/VestingServices";
+
+export default {
+  name: "VestingForm",
+  data() {
+    return {
+      loading: false,
+      valid: true,
+      address: null,
+      addressRules: [(v) => !!v || "Address is required"],
+    };
+  },
+  methods: {
+    submit() {
+      VestingServices.createAccount(this.address)
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log("Error! Could not reach the API. " + error);
+        });
+    },
+    validate() {
+      this.$refs.form.validate();
+    },
+  },
+};
+</script>
+
+<style scoped>
+.v-form {
+  text-align: center;
+}
+.v-input {
+  display: inline-flex;
+  width: 50%;
+  margin-right: 20px;
+  padding-top: 0;
+}
+
+.v-application .primary {
+  background-color: #399e97 !important;
+}
+</style>
