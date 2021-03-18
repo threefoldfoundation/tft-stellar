4c09f081245255da1f7112bc990467f9cb9d5dcf
ranatrk <rana.t.hassan@gmail.com>
2021-03-18 11:50:43 +0200

    Add models in models dir in package

--- ThreeBotPackages/vesting_dashboard/actors/vesting_dashoard.py
+++ ThreeBotPackages/vesting_dashboard/actors/vesting_dashoard.py
@@ -7,22 +7,25 @@ from stellar_sdk.exceptions import Ed25519PublicKeyInvalidError, BadRequestError
 from jumpscale.core.exceptions import JSException
 from jumpscale.loader import j
 from jumpscale.servers.gedis.baseactor import BaseActor, actor_method
-
-from .models import VestingEntry
 from jumpscale.core.base import StoredFactory
+
+current_full_path = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(current_full_path + "/../models/")
+from models import VestingEntry
+
+vesting_entry_model = StoredFactory(VestingEntry)
+vesting_entry_model.always_reload = True
 
 
 class VestingDashboard(BaseActor):
     @actor_method
     def create_escrow_account(self, owner_address: str) -> str:
-        vesting_factory = StoredFactory(VestingEntry)
         # TODO
         vesting_account = ""
         j.data.serializers.json.dumps({"data": vesting_account})
 
     @actor_method
     def list_vesting_accounts(self, owner_address: str) -> str:
-        vesting_factory = StoredFactory(VestingEntry)
         # TODO
         vesting_accounts = []
         j.data.serializers.json.dumps({"data": vesting_accounts})
