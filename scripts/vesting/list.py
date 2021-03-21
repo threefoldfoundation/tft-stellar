c7839bf9b03a584ec9b89fff08fa151768395880
Rob Van Mieghem <robvanmieghem@gmail.com>
2021-03-21 12:00:56 +0100

    small improvement to the vesting account listing output

--- scripts/vesting/list.py
+++ scripts/vesting/list.py
@@ -23,7 +23,7 @@ def list_vesting_accounts(network):
     vesting_accounts = get_vesting_accounts(network, "TFT")
 
     for vesting_account in vesting_accounts:
-        print(f"{vesting_account['account']} has {vesting_account['amount']} TFT with vesting scheme {vesting_account['scheme']}")
+        print(f"{vesting_account['account']} has {vesting_account['amount']} TFT with vesting scheme '{vesting_account['scheme']}'")
         if len(vesting_account["preauth_signers"])==0:
             continue 
         unlockhash = vesting_account["preauth_signers"][0]
@@ -33,7 +33,7 @@ def list_vesting_accounts(network):
         except requests.exceptions.HTTPError as e:
             if e.response.status_code == 404:
                 print(
-                    f"Unlockhash {unlockhash} for vesting account {vesting_account['account']} not found",
+                    f"Clean-up transaction with hash {unlockhash} for vesting account {vesting_account['account']} not found",
                     file=sys.stderr,
                 )
 
