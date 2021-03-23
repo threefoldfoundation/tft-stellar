2742de47736a80f34c65c106eb5b04c5f153fa3a
ranatrk <rana.t.hassan@gmail.com>
2021-03-23 13:27:43 +0200

    Get network from env variables when starting vesting dashboard if not passed in qwargs

--- ThreeBotPackages/vesting_dashboard/package.py
+++ ThreeBotPackages/vesting_dashboard/package.py
@@ -1,4 +1,5 @@
 from jumpscale.loader import j
+import os
 
 wallet_name = "vesting_temp_wallet"
 
@@ -9,8 +10,9 @@ class vesting_dashboard:
         wallet = None
         wallet_name = "vesting_temp_wallet"  # Only to be used to check balance of other wallets, no need to activate
         if "vesting_temp_wallet" not in j.clients.stellar.list_all():
-            wallet_network = kwargs.get("wallet_network", "STD")
-            print(wallet_network)
+            wallet_network = kwargs.get("wallet_network", None)
+            if not wallet_network:
+                wallet_network = os.environ.get("TFT_SERVICES_NETWORK", "STD")
             wallet = j.clients.stellar.new(wallet_name, network=wallet_network)
             wallet.save()
 
