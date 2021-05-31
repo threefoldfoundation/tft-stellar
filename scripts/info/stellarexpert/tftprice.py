132fa39841cca723b7ddfe016e683cb92e7ca513
Rob Van Mieghem <robvanmieghem@gmail.com>
2021-05-31 09:13:05 +0200

    Add the possibility for an enddate in the stellar expert price calculation

--- scripts/info/stellarexpert/tftprice.py
+++ scripts/info/stellarexpert/tftprice.py
@@ -15,11 +15,14 @@ def xlm_price_at(xlm_usd_values, timestamp):
         i += 1
 
 
-
 # May 2021 timestamp: 1619827200
+# June 21021 timestamp: 1622419200
 @click.command(help="Calculate the tft price using stellar.expert")
 @click.argument("starttimestamp", required=False, type=int, default=0)
-def tft_price_command(starttimestamp):
+@click.argument("endtimestamp", required=False, type=int, default=0)
+def tft_price_command(starttimestamp, endtimestamp):
+    if endtimestamp == 0:
+        endtimestamp = int(time.time()) + 3600
     # Get xlm prices in USD
     r = requests.get("https://api.stellar.expert/explorer/public/xlm-price")
     r.raise_for_status()
@@ -39,11 +42,12 @@ def tft_price_command(starttimestamp):
     relevant_history = [
         {"ts": i["ts"], "price": i["price"], "traded_amount": i["traded_amount"]} for i in history if "price" in i
     ]
+
     weighted_average_price = 0.0
     total_volume = 0.0
     for i in relevant_history:
         timestamp = i["ts"]
-        if timestamp < starttimestamp:
+        if timestamp < starttimestamp or timestamp > endtimestamp:
             continue
         xlm_price = xlm_price_at(xlm_usd_values, timestamp)
         high = i["price"][1]
@@ -55,9 +59,12 @@ def tft_price_command(starttimestamp):
         weighted_average_price += average * volume
         total_volume += volume
 
-        print(f"{datetime.datetime.fromtimestamp(timestamp)} high: {usd_high:.5f} USD low: {usd_low:.5f} USD volume: {volume}")
+        print(
+            f"{datetime.datetime.fromtimestamp(timestamp)} high: {usd_high:.5f} USD low: {usd_low:.5f} USD volume: {volume}"
+        )
+
     weighted_average_price = weighted_average_price / total_volume
-    print(f"Weighted average price= { weighted_average_price:.5f}")
+    print(f"Weighted average price = { weighted_average_price:.5f}")
 
 
 if __name__ == "__main__":
