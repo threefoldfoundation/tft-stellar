ca4b6d5eb0328186abf3d5cec8a584275655565a
Rob Van Mieghem <robvanmieghem@gmail.com>
2023-03-16 10:33:38 +0100

    Simplify the list vesting account script

--- scripts/vesting/list.py
+++ scripts/vesting/list.py
@@ -1,19 +1,12 @@
 #!/usr/bin/env python
 # pylint: disable=no-value-for-parameter
 import click
-import stellar_sdk
-import datetime
 import os
 import sys
-import requests
-import json
-
-from urllib import parse
-
 
 current_full_path = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(current_full_path + "/../../lib/stats/")
-from stats import get_vesting_accounts, get_unlockhash_transaction
+from stats import get_vesting_accounts 
 
 
 @click.command(help="Lists the TFT vesting accounts")
@@ -24,20 +17,8 @@ def list_vesting_accounts(network):
 
     for vesting_account in vesting_accounts:
         print(
-            f"{vesting_account['account']} has {vesting_account['amount']} TFT with vesting scheme '{vesting_account['scheme']}'"
+            f"{vesting_account['account']} has {vesting_account['amount']} TFT'"
         )
-        if len(vesting_account["preauth_signers"]) == 0:
-            continue
-        unlockhash = vesting_account["preauth_signers"][0]
-        try:
-            transaction = get_unlockhash_transaction(network, unlockhash)
-            print(f"Clean up transaction:\n{json.dumps(transaction)}")
-        except requests.exceptions.HTTPError as e:
-            if e.response.status_code == 404:
-                print(
-                    f"Clean-up transaction with hash {unlockhash} for vesting account {vesting_account['account']} not found",
-                    file=sys.stderr,
-                )
 
 
 if __name__ == "__main__":
