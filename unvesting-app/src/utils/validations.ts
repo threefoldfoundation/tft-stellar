c1cc55fb991293882f011e4839cd3836ae2c4a86
Thunder <mahmmoud.hassanein@gmail.com>
2023-03-28 13:00:58 +0200

    Added a validation helper to validate the private key.

--- unvesting-app/src/utils/validations.ts
+++ unvesting-app/src/utils/validations.ts
@@ -23,18 +23,34 @@ export const validateAddress = (
       isValid: false,
       errorMessage: 'Please enter a valid address.',
     };
-  } else {
-    return {
-      isValid: true,
-      errorMessage: '',
-    };
   }
+  return {
+    isValid: true,
+    errorMessage: '',
+  };
 };
 
 export const validatePrivateKey = (value: string) => {
-  console.log( value );  
+  if (value === '') {
+    return {
+      isValid: false,
+      errorMessage: 'Private key is required!',
+    };
+  }
+  if (value && value.length < 20) {
+    return {
+      isValid: false,
+      errorMessage: 'Private key should be > 20 char.',
+    };
+  }
+  if (!StrKey.isValidEd25519SecretSeed(value)) {
+    return {
+      isValid: false,
+      errorMessage: 'The private Key seems to be not valid.',
+    };
+  }
   return {
     isValid: true,
-    errorMessage: 'Please enter a valid private key.',
+    errorMessage: '',
   };
 };
