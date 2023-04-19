65ec958638f77d9ff8cd1779ddc0c542628b1273
Rob Van Mieghem <robvanmieghem@gmail.com>
2023-04-19 14:19:47 +0200

    Fix eslint errors

--- unvesting-app/src/utils/helpers.ts
+++ unvesting-app/src/utils/helpers.ts
@@ -7,26 +7,27 @@ const noConfigs = () => {
 };
 
 export const setConfig = () => {
-  const network: string = window.STELLAR_NETWORK
+  const network: string = window.STELLAR_NETWORK;
 
   switch (network) {
-    case 'test':
-      window.config = {
-        STELLAR_NETWORK: network,
-        STELLAR_HORIZON_URL: 'https://horizon-testnet.stellar.org',
-        SERVER_API_URL:
+  case 'test':
+    window.config = {
+      STELLAR_NETWORK: network,
+      STELLAR_HORIZON_URL: 'https://horizon-testnet.stellar.org',
+      SERVER_API_URL:
           'https://testnet.threefold.io/threefoldfoundation/vesting_service/',
-      };
-      break;
-    case 'main':
-      window.config = {
-        STELLAR_NETWORK: network,
-        STELLAR_HORIZON_URL: 'https://horizon.stellar.org',
-        SERVER_API_URL:
+    };
+    break;
+  case 'main':
+    window.config = {
+      STELLAR_NETWORK: network,
+      STELLAR_HORIZON_URL: 'https://horizon.stellar.org',
+      SERVER_API_URL:
           'https://tokenservices.threefold.io/threefoldfoundation/vesting_service/',
-      };
-      break;
-    default:
-      noConfigs();
+    };
+    break;
+  default:
+    noConfigs();
+    break;
   }
 };
