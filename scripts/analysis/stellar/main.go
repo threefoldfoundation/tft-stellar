6332cd28940f338dad3ceac3490c699ee134724e
Rob Van Mieghem <robvanmieghem@gmail.com>
2023-09-08 09:54:03 +0200

    Ignore SetTrustLineFlags operations in the Stellar TFT analysis

--- scripts/analysis/stellar/main.go
+++ scripts/analysis/stellar/main.go
@@ -91,6 +91,8 @@ func main() {
 							continue
 						} else if _, ok := op.(*txnbuild.AllowTrust); ok {
 							continue
+						} else if _, ok := op.(*txnbuild.SetTrustLineFlags); ok {
+							continue
 						} else if _, ok := op.(*txnbuild.BeginSponsoringFutureReserves); ok {
 							continue
 						} else if _, ok := op.(*txnbuild.EndSponsoringFutureReserves); ok {
